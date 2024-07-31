661535cf9677138356e04adee8cc9b443d5d1034
longliangzhe <168954597+longliangzhe@users.noreply.github.com>
2024-07-31 08:57:36 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open('README.md','r',encoding='utf-8') as f:
     long_des = f.read()
 setuptools.setup(
     name='suffix_expression',
-    version='0.0.1',
+    version='0.0.2',
     author='Lighting Long',
     author_email='17818883308@139.com',
     description='后缀表达式处理库',
